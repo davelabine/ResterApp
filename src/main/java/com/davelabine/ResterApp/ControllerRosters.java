f03d3aaf3a7f95a4cdbd999d1f1b45db07989d58
davelabine <davelabine@gmail.com>
2016-05-22 14:47:27 -0700

    More file and webapp tinkering.

--- src/main/java/com/davelabine/ResterApp/ControllerRosters.java
+++ src/main/java/com/davelabine/ResterApp/ControllerRosters.java
@@ -6,10 +6,10 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 /**
- * Root resource (exposed at "rosters" path)
+ * Root resource (exposed at "/rosters" path)
  */
-@Path("rosters")
-public class MyResource {
+@Path("/rosters")
+public class ControllerRosters {
 
     /**
      * Method handling HTTP GET requests. The returned object will be sent
@@ -20,6 +20,6 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "Got it!";
+        return "Got a Roster!";
     }
 }
