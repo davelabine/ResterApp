db0e7afe4b31f551f59b8bf7dd2c23cabac60dd6
David LaBine <davelabine@gmail.com>
2017-08-16 05:39:13 -0700

    A little style cleanup and a data example in the comments so I can start hooking up props.

--- student-app/src/components/filterableStudentList/FilterableStudentList.tsx
+++ student-app/src/components/filterableStudentList/FilterableStudentList.tsx
@@ -5,6 +5,74 @@ export interface Props {
   name: string;
 }
 
+/*
+[
+    {
+        "skey": "8a80810d5dd89a3f015dd89ac8280000",
+        "id": "BIlly Bob",
+        "name": "1234",
+        "photo": {
+            "bucketName": "resterapp-dev",
+            "key": "e93ecbf0-ba96-4290-a906-5ea67e4c4a9f"
+        }
+    },
+    {
+        "skey": "8a80810d5dd89a3f015dd8a252800001",
+        "id": "503074",
+        "name": "Albert Russell",
+        "photo": null
+    },
+    {
+        "skey": "8a80810d5dd89a3f015dd8a259b2000e",
+        "id": "942226",
+        "name": "Alfredo Cummings",
+        "photo": null
+    },
+    {
+        "skey": "8a80810d5dd89a3f015dd8a2677f0027",
+        "id": "824388",
+        "name": "Allen Russell",
+        "photo": null
+    },
+    {
+        "skey": "8a80810d5dd89a3f015dd8a271fb003a",
+        "id": "266192",
+        "name": "Andre McKenna",
+        "photo": null
+    },
+    {
+        "skey": "8a80810d5dd89a3f015dd8a27f0d0052",
+        "id": "932083",
+        "name": "Angelica Obrien",
+        "photo": null
+    },
+    {
+        "skey": "8a80810d5dd89a3f015dd8a281c80057",
+        "id": "682307",
+        "name": "Anna Washington",
+        "photo": null
+    },
+    {
+        "skey": "8a80810d5dd89a3f015dd8a27c52004d",
+        "id": "734552",
+        "name": "Antonio Fowler",
+        "photo": null
+    },
+    {
+        "skey": "8a80810d5dd89a3f015dd8a25fb80019",
+        "id": "615643",
+        "name": "Ashley Mcdaniel",
+        "photo": null
+    },
+    {
+        "skey": "8a80810d5dd89a3f015dd8a25897000c",
+        "id": "753238",
+        "name": "Beatrice Russell",
+        "photo": null
+    }
+]
+*/
+
 class FilterableStudentList extends React.Component<Props, object> {
   render() {
     const { name } = this.props;
@@ -16,25 +84,26 @@ class FilterableStudentList extends React.Component<Props, object> {
               <label>Search Last Name: </label>
               <input type="text" name="name"/>
               <button type="button" className="btn btn-primary">Search</button>
+              <button type="button" className="btn btn-default pull-right">Add Student</button>
           </form>
+          <table className="table table-striped table-sm table-hover">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>ID</th>
+                <th>URL</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{name}</td>
+                <td>12345</td>
+                <td><a href="http://google.com">view</a> | 
+                <a href="http://google.com"> edit</a></td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-        <table className="table table-striped table-sm table-hover">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>ID</th>
-              <th>URL</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{name}</td>
-              <td>12345</td>
-              <td><a href="http://google.com">view</a> | 
-              <a href="http://google.com"> edit</a></td>
-            </tr>
-          </tbody>
-        </table>
       </div>
     );
   }
